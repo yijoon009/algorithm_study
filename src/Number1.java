c2dd70c0d5d3d4998103f9230a4e5170569de95d
yjkim <yijoon009@gmail.com>
2022-04-10 21:50:18 +0900

    first commit

---
+++ src/Number1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Number1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String number = sc.next();
+            if(number.equals("0")){
+                break;
+            }
+            for(int i=0; i<number.length(); i++){
+
+            }
+
+        }
+        System.out.println("hello world");
+    }
+}
