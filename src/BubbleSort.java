86a684b101e6e585a0339da3b110e8c2f8e03ff5
yijoon009 <74906042+yijoon009@users.noreply.github.com>
2022-04-22 15:08:20 +0900

    bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,7 +5,7 @@
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] numbers = {4,7,9,0,2,3,1,6,11,16,84, 55,32,18};
+        int[] numbers = {8,3,7,5,4,2};
         for(int i=1; i<numbers.length; i++){            //(총 배열 크기-1 만큼 돌기)
             for(int j=0; j<numbers.length-1; j++){      //각 비교는 점점 줄어들기
                 if(numbers[j] > numbers[j+1]){
@@ -14,9 +14,10 @@ public class BubbleSort {
                     numbers[j+1] = tmp;
                 }
             }
-        }
-        for (int num : numbers){
-            System.out.println(num);
+            for(int num : numbers){
+                System.out.print(num + " ");
+            }
+            System.out.println();
         }
     }
 }
