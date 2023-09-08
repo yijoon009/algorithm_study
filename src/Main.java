2c27480b66a5f6a8f97282cbb53cd3093ff827f6
yijoon009 <yijoon009@gmail.com>
2023-09-08 22:34:44 +0900

    Q10171

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
-
-        // 여기서 문제풀이
-
-
-
-
-        
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
     }
 }
+
+
+
+
